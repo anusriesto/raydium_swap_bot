b6e72fa725a3469d183e907ab215d246cdf5e306
anusriesto <anujwrap@live.com>
2024-09-17 18:10:06 +0530

    balance and creating wallet functions

---
+++ src/create_wallet.ts
@@ -0,0 +1,68 @@
+import { Keypair,PublicKey,Connection,LAMPORTS_PER_SOL,clusterApiUrl } from "@solana/web3.js";
+import * as fs from "fs";
+import dotenv from 'dotenv';
+import https from 'https';
+import base58 from 'bs58';
+dotenv.config();
+
+const generateKey = async (): Promise<void> => {
+  //generating new wallet
+  const keyPair: Keypair = Keypair.generate();
+  const keyData = Array.from(keyPair.secretKey);  // Convert Uint8Array to regular array;
+
+  console.log("Public Key:", keyPair.publicKey.toString());
+  console.log("Secret Key:", keyPair.secretKey); // Secret key is a UInt8Array
+  fs.writeFileSync("secret-key.json", JSON.stringify(keyData));
+
+  
+
+};
+//Writing few helper function
+  //******************************************
+  //Getting public key
+
+const getPublicKeyFromJson = (filePath: string): PublicKey => {
+    // Read the JSON file
+    const keyData = fs.readFileSync(filePath, "utf8");
+  
+    // Parse the JSON data
+    const parsedData = JSON.parse(keyData);
+  
+    if (!parsedData.secretKey) {
+      throw new Error("Secret key not found in the JSON file.");
+    }
+  
+    // Convert the secret key (array) to Uint8Array
+    const secretKey = Uint8Array.from(parsedData.secretKey);
+  
+    // Generate the Keypair from the secret key
+    const keypair = Keypair.fromSecretKey(secretKey);
+  
+    // Return the public key
+    return keypair.publicKey;
+};
+
+
+//Get Balance of the wallet
+
+const get_balance = async (): Promise<void> => {
+    if (!process.env.QUICKNODE_URL) {
+        throw new Error('QUICKNODE_URL is not set in the environment variables');
+      }
+    const url = new URL(process.env.QUICKNODE_URL);
+    if (!process.env.WALLET_SECRET_KEY) {
+        throw new Error('WALLET_SECRET_KEY is not provided');
+      }
+      const secretKey = base58.decode(WALLET_SECRET_KEY);
+      if (secretKey.length !== 64) {
+        throw new Error('Invalid secret key length. Expected 64 bytes.');
+      }
+      this.wallet = new Wallet(Keypair.fromSecretKey(secretKey));
+};
+
+  
+  // Example usage:
+const publicKey = getPublicKeyFromJson("secret-key.json");
+console.log("Public Key:", publicKey.toString());
+
+generateKey();
